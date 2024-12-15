496d5fc8a40853b2c821e2c994b89d7491f91fea
guiogigo <guilhermevbatista20@gmail.com>
2024-12-14 23:17:43 -0300

    Noite 1412

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -2,7 +2,6 @@ import { Routes, Route, Navigate } from "react-router-dom";
 
 import { Home } from '../pages/Home'
 import { Dish } from '../pages/Dish'
-import { SignIn } from "../pages/SignIn";
 
 export function AppRoutes() {
     return(
@@ -11,7 +10,6 @@ export function AppRoutes() {
             <Route path="/home" element={<Home/>}/>
 
             <Route path="/dishes/:id" element={<Dish/>}/>
-            <Route path="/sla" element={<SignIn/>}/>
 
             <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
